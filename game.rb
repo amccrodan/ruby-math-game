854cec96783d5d6632a4c92f236ffb919f413d18
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-02-14 01:44:19 +0000

    Created file and set Game to initialize with 2 players, and set P1 to current_player. Can toggle player and get lives of each.

---
+++ game.rb
@@ -0,0 +1,21 @@
+module MathGame
+  class Game
+    attr_reader :current_player
+
+    def initialize(player1, player2)
+      @player1 = player1
+      @player2 = player2
+      @current_player = player1
+      # p @player1, @player2, @current_player
+    end
+
+    def toggle_player
+      @current_player = @current_player == @player1 ? @player2 : @player1
+    end
+
+    def current_lives
+      [@player1.lives, @player2.lives]
+    end
+
+  end
+end
