59ff999486917dd20871b2374c6b85bb1c3485df
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-02-14 01:47:24 +0000

    Made question #s read-only, and changed ask_question to only check the answer to the question.

--- question.rb
+++ question.rb
@@ -1,14 +1,13 @@
 module MathGame
   class Question
-    attr_accessor :num1, :num2
+    attr_reader :num1, :num2
 
     def initialize
       @num1 = rand(1..20)
       @num2 = rand(1..20)
     end
 
-    def ask_question
-      puts "What is #{@num1} + #{@num2}?"
+    def check_question
       answer = gets.chomp.to_i
       answer == @num1 + @num2
     end
