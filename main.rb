9d67809ca0a878e9cee60ecbb33df5ac904ee37c
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-02-14 02:06:43 +0000

    Extracted some logic and added newlines to make output nicer.

--- main.rb
+++ main.rb
@@ -4,35 +4,34 @@ module MathGame
     require './game'
     require './question'
 
-    game1 = Game.new(Player.new(1), Player.new(2))
+    game1 = Game.new(Player.new("Player 1"), Player.new("Player 2"))
     game_over = false
 
-    until game_over do
+    until game1.over? do
       puts "------- NEW TURN -------"
       print game1.current_player.title
       q = Question.new
       puts ": What is #{q.num1} + #{q.num2}?"
 
-      if q.check_question
-        puts "Good work!"
+      if q.check_question(gets.chomp)
+        puts "Good work!\n\n"
       else
         puts "No way, dumbo!"
         game1.current_player.lose_a_life
 
-        if game1.current_player.lives == 0
-          game_over = true
+        if game1.over?
           game1.toggle_player
           next
         end
 
         life_arr = game1.current_lives
-        puts "P1: #{life_arr[0]}/3 vs. P2: #{life_arr[1]}/3"
+        puts "P1: #{life_arr[0]}/3 vs. P2: #{life_arr[1]}/3\n\n"
       end
 
       game1.toggle_player
     end
 
-    puts "#{game1.current_player.title} is the winner!"
+    puts "\n#{game1.current_player.title} is the winner!"
     puts "------- GAME OVER --------"
 
   end
